dc76a7d341b2f87016bd31b391c408351a7efc5c
get-Friday <gabi.get@hotmail.com>
2022-05-06 11:10:10 -0300

    service product

--- src/app/models/product.models.ts
+++
@@ -1,9 +0,0 @@
-interface Product {
-  title: string;
-  description: string;
-  photo: string;
-  price: number;
-  type: string;
-}
-
-export default Product
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import IDrink from '../models/drink.model';
+import IFood from '../models/food.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  purchase: IDrink[] | IFood[] = []
+
+  constructor() { }
+
+  getPurchase() {
+    return this.purchase.length
+  }
+
+  insertProduct(product: IDrink | IFood) {
+    this.purchase.push(product)
+  }
+
+  clearPurchase() {
+    this.purchase = []
+  }
+
+  removeProduct(product: IDrink | IFood) {
+    const productIndex = this.purchase.findIndex(e => e.name == product.name)
+    this.purchase.splice(productIndex, 1)
+  }
+}
